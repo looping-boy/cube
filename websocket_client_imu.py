9a72d14b56c34ee88b7eec9e4f083f1d0cf93cc3
Lotis <guillaume.lotis@gmail.com>
2023-07-30 23:07:36 +0200

    OK, Now everything works ! The python in the git drives the cube on the website

--- websocket_client_imu.py
+++ websocket_client_imu.py
@@ -3,22 +3,48 @@ import random
 import json
 import websockets
 
+import asyncio
+import websockets
+import json
+import random
+
 async def send_imu_data():
     i = 0
-    async with websockets.connect('wss://cube-app-ded2c0470b54.herokuapp.com/') as websocket:
+    async with websockets.connect("wss://cube-app-ded2c0470b54.herokuapp.com/") as websocket:
+        current_rotation = {
+            'gyro_x': 0.0,
+            'gyro_y': 0.0,
+            'gyro_z': 0.0
+        }
+
         while True:
-            imu_data = {
+            target_rotation = {
                 'gyro_x': random.uniform(-1.0, 1.0),
                 'gyro_y': random.uniform(-1.0, 1.0),
                 'gyro_z': random.uniform(-1.0, 1.0)
             }
 
-            # Convert to JSON and send to the server
-            await websocket.send(json.dumps(imu_data))
-            print("\rSending data", i, end="")
-            i += 1
+            duration = 30  # Number of frames for the transition
+            for frame in range(duration):
+                # Calculate the intermediate rotation using the easing function
+                t = frame / duration
+                eased_rotation = {}
+                for axis in ['gyro_x', 'gyro_y', 'gyro_z']:
+                    eased_rotation[axis] = current_rotation[axis] + (target_rotation[axis] - current_rotation[axis]) * t
 
-            # Wait for some time before sending the next data
-            await asyncio.sleep(0.2)
+                # Convert to JSON and send to the server
+                await websocket.send(json.dumps(eased_rotation))
+                print("\rSending data", i, end="")
+                i += 1
+
+                # Wait for some time before sending the next frame
+                await asyncio.sleep(0.02)
+
+            # Update the current rotation to the target rotation after the transition
+            current_rotation = target_rotation
 
 asyncio.run(send_imu_data())
+
+
+# THE WEBSITE THAT DRIVES THE CUBE WHEN YOU LAUNCH THIS PY SCRIPT :
+# https://cube-app-ded2c0470b54.herokuapp.com/
