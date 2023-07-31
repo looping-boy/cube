dba51fa3bb1c92da63207ee052503b07f993cac5
Lotis <guillaume.lotis@gmail.com>
2023-07-31 11:49:27 +0200

    add test to see if server gets datas (TO TEST)

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
+const dgram = require('dgram');
 const app = express();
+const WebSocket = require('ws');
 
 app.use(express.static('public'));
 
 const server = http.createServer(app);
+
+// Create a UDP server
+const udpServer = dgram.createSocket('udp4');
+
+udpServer.on('error', (err) => {
+  console.log(`UDP server error:\n${err.stack}`);
+  udpServer.close();
+});
+
+udpServer.on('message', (message, rinfo) => {
+  console.log(`Received UDP message from client: ${message}`);
+  // Broadcast the received message to all connected WebSocket clients
+  wss.broadcast(`${message}`);
+});
+
+udpServer.on('listening', () => {
+  const address = udpServer.address();
+  console.log(`UDP server listening on ${address.address}:${address.port}`);
+});
+
+udpServer.bind(5001); // UDP server listens on port 5001
 
 const wss = new WebSocket.Server({ server });
 
@@ -16,7 +38,7 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     console.log(`Received message from client: ${message}`);
-    wss.broadcast(`${message}`);
+    // If needed, you can handle WebSocket messages here
   });
 
   ws.on('close', () => {
@@ -31,7 +53,7 @@ wss.broadcast = function broadcast(msg) {
     });
 };
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 server.listen(PORT, () => {
   console.log(`Server started and listening on port ${PORT}`);
 });
--- websocket_client_imu.py
+++ websocket_client_imu.py
@@ -1,50 +1,46 @@
 import asyncio
 import random
+import socket
 import json
-import websockets
-
-import asyncio
-import websockets
-import json
-import random
 
 async def send_imu_data():
     i = 0
-    async with websockets.connect("wss://cube-app-ded2c0470b54.herokuapp.com/") as websocket:
-        current_rotation = {
-            'gyro_x': 0.0,
-            'gyro_y': 0.0,
-            'gyro_z': 0.0
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    destination_ip = '127.0.0.1'
+    destination_port = 5001
+
+    current_rotation = {
+        'gyro_x': 0.0,
+        'gyro_y': 0.0,
+        'gyro_z': 0.0
+    }
+
+    while True:
+        target_rotation = {
+            'gyro_x': random.uniform(-1.0, 1.0),
+            'gyro_y': random.uniform(-1.0, 1.0),
+            'gyro_z': random.uniform(-1.0, 1.0)
         }
 
-        while True:
-            target_rotation = {
-                'gyro_x': random.uniform(-1.0, 1.0),
-                'gyro_y': random.uniform(-1.0, 1.0),
-                'gyro_z': random.uniform(-1.0, 1.0)
-            }
+        duration = 30  # Number of frames for the transition
+        for frame in range(duration):
+            # Calculate the intermediate rotation using the easing function
+            t = frame / duration
+            eased_rotation = {}
+            for axis in ['gyro_x', 'gyro_y', 'gyro_z']:
+                eased_rotation[axis] = current_rotation[axis] + (target_rotation[axis] - current_rotation[axis]) * t
 
-            duration = 30  # Number of frames for the transition
-            for frame in range(duration):
-                # Calculate the intermediate rotation using the easing function
-                t = frame / duration
-                eased_rotation = {}
-                for axis in ['gyro_x', 'gyro_y', 'gyro_z']:
-                    eased_rotation[axis] = current_rotation[axis] + (target_rotation[axis] - current_rotation[axis]) * t
+            # Convert to JSON and send to the server
+            message = json.dumps(eased_rotation).encode()
+            udp_socket.sendto(message, (destination_ip, destination_port))
 
-                # Convert to JSON and send to the server
-                await websocket.send(json.dumps(eased_rotation))
-                print("\rSending data", i, end="")
-                i += 1
+            print("\rSending data", i, end="")
+            i += 1
 
-                # Wait for some time before sending the next frame
-                await asyncio.sleep(0.02)
+            # Wait for some time before sending the next frame
+            await asyncio.sleep(0.02)
 
-            # Update the current rotation to the target rotation after the transition
-            current_rotation = target_rotation
+        # Update the current rotation to the target rotation after the transition
+        current_rotation = target_rotation
 
 asyncio.run(send_imu_data())
-
-
-# THE WEBSITE THAT DRIVES THE CUBE WHEN YOU LAUNCH THIS PY SCRIPT :
-# https://cube-app-ded2c0470b54.herokuapp.com/
